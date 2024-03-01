3bfb2281ddcc20db8fb3db216a31920e00ed3311
Abbos <abbos.pankiy2018@gmail.com>
2024-03-01 23:18:26 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//////////////////////////////////////////////////////////////////////
+// Ohirgi harfni 'l'ni true qilib chiqarish //
+// let title = 'Fozil';
+
+// let ending = title.endsWith ('l')
+// console.log(ending);
+
+
+//////////////////////////////////////////////////////////////////////
+// O'zgatuvchilar qiymatini almashtirish //
+// let str0 = 'Web';
+// let str1 = 'Brain';
+
+// let replacingstrfirst = str0.replace ('Web','Brain')
+// let replacingstrsecond = str1.replace ('Brain', 'Web')
+
+// console.log(replacingstrfirst);
+// console.log(replacingstrsecond);
+
+///////////////////////////////////////////////////////////////////////
+// Kabis yilni topish //
+
+// let year = 2020;
+// if( year % 4 === 0){
+//     console.log('Kabisa yil');
+// } else if ( year % 4 !== 0){
+//     console.log('Kabisa yili emas');
+// }
+
+///////////////////////////////////////////////////////////////////////
+// # ni () qavslarga aylantirish //
+
+// let title = '#fozil#web#brain';
+
+// let replacing = title.replace ('#fozil#web#brain', '(fozil)web(brain)')
+// console.log(replacing);
+
+
+///////////////////////////////////////////////////////////////////////
+// sozni 10ta qilib, webni IT so'ziga almashtirish //
+// let title = 'Webbrain';
+
+// let repeating = title.repeat (10);
+// let replacing = title.replace ('Webbrain', 'ITBrain');
+
+// console.log(repeating);
+// console.log(replacing);
